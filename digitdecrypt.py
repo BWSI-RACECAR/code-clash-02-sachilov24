ba52752dd60852975b10bc1405cc7d6cd021bd7b
sachilov24 <111238948+sachilov24@users.noreply.github.com>
2023-07-13 09:05:26 -0400

    Add files via upload

---
+++ digitdecrypt.py
@@ -0,0 +1,64 @@
+"""
+Copyright MIT BWSI Autonomous RACECAR Course
+MIT License
+Summer 2023
+
+Code Clash #2 - Digit Decrypt (digitdecrypt.py)
+
+
+Author: Koneshka Bandyopadhyay
+
+Difficulty Level: 4/10
+
+Prompt: Koneshka has a collection of 9 RACECARS, each with unique speed and a crypted 
+identification number that helps identify the rankings of each of the RACECARS in terms
+of speed. Write a program that helps identify the ranking of a car given the identification
+number. The way we can decrypt this identification number is by adding digits of the number 
+until we reach single digits. For example: Let’s say that the identification number is 38. 
+Then, we add ‘3’ + ‘8’ to get 11. We further add the digits ‘1’+ ‘1’ to get 2, ranking the car
+in 2nd place or 2nd fastest out of 9. Car with identification 1111 gives us 1+1+1+1=4, ranking 
+the car 4th fastest out of 9. The goal is to add the digits until we reach single digits.
+
+Test Cases:
+Input: 48 Output: 3
+
+Input: 13 Output: 4
+
+Input: 0 Output: 0 
+"""
+
+class Solution:    
+    def digitdecrypt(self, num):
+            #type num: int
+            #return type: int
+            
+            #TODO: Write code below to returnn an int with the solution to the prompt.
+            pass
+        def find_number(x):
+    Decode = ''
+    count = ''
+    for i in x:
+        
+        if i.isdigit():
+            #append to count
+            count += i
+        else i: 
+   
+            decode += i * int(count)
+            count = '' 
+            
+         
+    return decode
+
+ 
+def main():
+    input1= input()
+    input2 = int(input1)
+
+    
+    tc1 = Solution()
+    ans = tc1.digitdecrypt(input2)
+    print(ans)
+    
+if __name__ == "__main__":
+    main()
